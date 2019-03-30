f40f3196df9506e48ec2f21d591174c02653570d
abhishek <abhishek.satapathy01@gmail.com>
2019-03-30 16:22:42 +0530

    added http port

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def vis():
 
 
 if __name__ == "__main__":
-	app.run(debug = True, host='0.0.0.0', port=8000, passthrough_errors=True)
+	app.run(debug = True, host='0.0.0.0', port=80, passthrough_errors=True)
